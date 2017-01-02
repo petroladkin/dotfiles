44de5f17f7b8c08069cabda9e76aac99a3964927
Petro Ladkin <petro@auxoft.com>
2017-01-02 19:19:45 +0200

    add zsh config

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 rm    ~/.vimrc
 rm -R ~/.vim
 rm    ~/.tmux.conf
+rm    ~/.zshrc
 
 rpwd="$(pwd)"
 
 ln -s "$rpwd/vimrc"     ~/.vimrc 
 ln -s "$rpwd/vim"       ~/.vim
 ln -s "$rpwd/tmux.conf" ~/.tmux.conf
+ln -s "$rpwd/zshrc"     ~/.zshrc
 
