536dfa69472507c38fc5b58bf33f5b20ddc34960
Petro Ladkin <petro.ladkin@gmail.com>
2023-01-02 20:11:38 +0200

    Fix install.sh for OSX

--- tools/install.sh
+++ tools/install.sh
@@ -4,25 +4,23 @@ IS_OSX=1
 IS_FEDORA_LINUX=0
 IS_UBUNTU_LINUX=0
 #TODO: add check OSX
-if grep "^NAME=" /etc/os-release | grep -q "Fedora"; then
-  # Fedora
-  IS_OSX=0
-  IS_FEDORA_LINUX=1
-elif grep "^NAME=" /etc/os-release | grep -q "Ubuntu"; then
-  # Ubuntu
-  IS_OSX=0
-  IS_UBUNTU_LINUX=1
-else
-  grep "^NAME=" /etc/os-release
-  echo 'ERROR: unsupported OS'
-  exit 1
+if [[ -f "/etc/os-release" ]]; then
+  # Linux
+  if grep "^NAME=" /etc/os-release | grep -q "Fedora"; then
+    # Fedora
+    IS_OSX=0
+    IS_FEDORA_LINUX=1
+  elif grep "^NAME=" /etc/os-release | grep -q "Ubuntu"; then
+    # Ubuntu
+    IS_OSX=0
+    IS_UBUNTU_LINUX=1
+  else
+    grep "^NAME=" /etc/os-release
+    echo 'ERROR: unsupported OS'
+    exit 1
+  fi
 fi
 
-if [ $IS_OSX -eq 1 ]; then
-  echo "It's OSX"
-  echo 'ERROR: unsupported OS'
-  exit 1
-fi
 
 SUDO=""
 if command -v sudo &> /dev/null; then
@@ -38,6 +36,12 @@ if [ $IS_UBUNTU_LINUX -eq 1 ]; then
   echo ""
   $SUDO apt-get update && $SUDO apt-get install -y git
 fi
+
+if [ $IS_OSX -eq 1 ]; then
+  echo ""
+  brew install git
+fi
+
 
 if [ -d "$HOME/.dotfiles" ]; then
   echo "$HOME/.dotfiles directory exist"
